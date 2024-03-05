d4d1164a15c671717743ef69abfc4ac1bd35dd9e
Nghị <nghingothanhnghi@gmail.com>
2024-03-05 13:05:52 +0700

    update

--- src/components/LanguageSwitch/LanguageSwitch.jsx
+++ src/components/LanguageSwitch/LanguageSwitch.jsx
@@ -5,19 +5,13 @@ import { useTranslation } from 'react-i18next';
 
 const LanguageSwitch = () => {
   const { i18n } = useTranslation();
-  const [selected, setSelected] = useState(localStorage.getItem("selectedLanguage") || getDefaultLanguage());
+  const [selected, setSelected] = useState(i18n.language);
 
   const languageOptions = [
-    { value: "en", label: "EN" },
     { value: "vn", label: "VN" },
+    { value: "en", label: "EN" },
     // Add more options as needed
   ];
-
-  function getDefaultLanguage() {
-    // Set your logic to determine the default language here
-    // For example, you can use the browser's language or any other preference
-    return "vn";
-  }
 
   const handleSelect = (eventKey) => {
     const selectedOption = languageOptions.find((option) => option.value === eventKey);
@@ -27,12 +21,20 @@ const LanguageSwitch = () => {
   };
 
   useEffect(() => {
-    i18n.changeLanguage(selected);
-  }, [selected, i18n]);
+    const handleLanguageChange = () => {
+      setSelected(i18n.language);
+    };
+
+    i18n.on('languageChanged', handleLanguageChange);
+
+    return () => {
+      i18n.off('languageChanged', handleLanguageChange);
+    };
+  }, [i18n]);
 
   return (
     <>
-      <Dropdown onSelect={handleSelect} defaultactivekey={i18n.language || selected} align="end">
+      <Dropdown onSelect={handleSelect} defaultactivekey={selected} align="end">
         <Dropdown.Toggle variant="light" size="sm" className="px-3 no-caret rounded-pill">
           <IconWorld size={18} stroke={1}/> <span className="text-truncate">{languageOptions.find((option) => option.value === selected)?.label || "Select Language"}</span>
         </Dropdown.Toggle>
